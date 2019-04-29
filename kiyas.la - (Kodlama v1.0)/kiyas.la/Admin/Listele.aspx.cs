f3798be024a3311fe72b5972cf20359345c5d1a6
mduldul <mertcanduldul@gmail.com>
2019-04-29 14:01:21 +0300

    V1

---
+++ kiyas.la - (Kodlama v1.0)/kiyas.la/Admin/Listele.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using kiyas.la.Context;
+
+namespace kiyas.la.Admin
+{
+    public partial class Listele : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TelefonYükle();
+            }
+        }
+        private void TelefonYükle()
+        {
+            using (KiyaslaContext db = new KiyaslaContext())
+            {
+                var yükle = (from i in db.SmartPhone
+                             select new
+                             {
+                                 i.Id,
+                                 i.TelefonMarkasi,
+                                 i.TelefonModeli,
+                                 i.İslemciMarkasi,
+                                 i.İslemciModeli,
+                                 i.İslemciCekirdek,
+                                 i.İslemciHizi_Ghz,
+                                 i.İsletimSistemi,
+                                 i.Ekrancözünürlügü,
+                             }).ToList();
+                ÜrünListe.DataSource = yükle;
+                ÜrünListe.DataBind();
+            }
+
+        }
+    }
+}
+
+
+
+//<Columns>
+//            <asp:BoundField DataField="Id" HeaderText="Id" />
+//            <asp:BoundField DataField="TelefonMarkasi" HeaderText="Telefon Markasi" />
+//            <asp:BoundField DataField="TelefonModeli" HeaderText="Telefon Modeli" />
+//            <asp:BoundField DataField="İslemciMarkasi" HeaderText="İslemci Markası" />
+//            <asp:BoundField DataField="İslemciModeli" HeaderText="İslemci Modeli" />
+//            <asp:BoundField DataField="İslemciCekirdek" HeaderText="İslemci Kaç Çekirdek" />
+//            <asp:BoundField DataField="İslemciHizi_Ghz" HeaderText="İslemci Hizi" />
+//            <asp:BoundField DataField="İsletimSistemi" HeaderText="İsletim Sistemi" />
+//            <asp:BoundField DataField="Ekrancözünürlügü" HeaderText="Ekran Çözünürlügü" />
+//            <asp:TemplateField>
+//                <ItemTemplate>
+//                    <a href='Detay?id=<%#Eval("Id")%>'>Detay</a>
+//                </ItemTemplate>
+//            </asp:TemplateField>
+//        </Columns>
---
+++ kiyas.la - (Kodlama v1.0)/kiyas.la/Entities/Phone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kiyas.la.Entities
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        public string TelefonMarkasi { get; set; }
+
+        public string TelefonModeli { get; set; }
+
+        public string Ekrancözünürlügü { get; set; }
+
+        public double ArkaKamerapixel { get; set; }
+
+        public double ÖnKamerapixel { get; set; }
+
+        public string İslemciMarkasi { get; set; }
+
+        public string İslemciModeli { get; set; }
+
+        public double İslemciHizi_Ghz { get; set; }
+
+        public int İslemciCekirdek { get; set; }
+
+        public string GrafikİslemciModeli { get; set; }
+
+        public double GrafikİslemciHizi_Mhz { get; set; }
+
+        public double DahiliDepolama_GB { get; set; }
+
+        public int MicroSd_GB { get; set; }
+
+        public bool MicroSdVarmi { get; set; }
+
+        public int Batarya_Mh { get; set; }
+
+        public double Agırlık_Gram { get; set; }
+
+        public string Renk { get; set; }
+
+        public string İsletimSistemi { get; set; }
+
+        public string SistemSürümü { get; set; }
+
+    }
+}
